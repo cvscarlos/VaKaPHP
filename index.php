ae5a033bcf99640d6355ec3c5a101d8cd0c39560
caljp13 <soad89toxicity@yahoo.com.br>
2011-08-13 11:54:51 -0300

    Add project url in comments

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* VakaPHP Framework
+* VakaPHP Framework < http://github.com/caljp13/VakaPHP >
 * @author Carlos Vinicius <caljp13@gmail.com>
 *
 * This work is licensed under the Creative Commons Attribution 3.0 Unported License. To view a copy of this license,
